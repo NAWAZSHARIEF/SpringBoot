0d58cdfd47ef8723f989d94d40a3cc83150b409b
pirzada.sharief <pirzada.sharief@NGZ06011-186.newgen.co.in>
2019-11-04 20:50:07 +0530

    1st Commit

---
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,25 @@
+package io.javabrains.moviecatalogservice.resources;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.javabrains.moviecatalogservice.models.CatalogItem;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+	@RequestMapping("/{userId}")	
+	private List<CatalogItem> getCatalog(@PathVariable String userId){
+		
+		return Collections.singletonList(
+				new CatalogItem("Transformers", "TransTest", 5)
+				);
+		
+	} 
+	
+}
